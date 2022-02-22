b38fdf1acee25bd0c60b1c25187e103ff2c7944b
Tony Grimes <acidtone@tonygrimes.com>
2022-02-21 18:45:18 -0700

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// String
+const quote = "I'm a genius billionaire playboy philanthropist."
+console.log('---String---');
+console.log('Before:', quote);
+console.log('After:', quote.split(' '));
+
+// Number
+const ageUniverse = 13770000000;
+console.log('---Number---');
+console.log('Before:', ageUniverse);
+console.log('After:', `Age of the Universe: ${ageUniverse.toLocaleString()} years!`);
+
+// Boolean
+const yes = true;
+console.log('---Boolean---');
+console.log('Before:', yes);
+console.log('After:', !yes);
+
+// Array
+const hobbies = ['Kicking a$$', 'Chewing bubble gum'];
+console.log('---Array---');
+console.log('Before:', hobbies);
+console.log('After:', `I have ${hobbies.length} hobbies.`);
